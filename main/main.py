a858930fe1fb4df28aa79d4d5c310f6d33aa31bb
fiona <fiona.kitchen@yahoo.co.uk>
2022-12-13 16:14:05 +0000

    had a play with pretty table and pip actually worked??

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen 
+""" from turtle import Turtle, Screen 
 timmy = Turtle()
 timmy.shape('turtle')
 timmy.color('DeepPink')
@@ -6,4 +6,12 @@ print(timmy)
 timmy.forward(200)
 
 my_screen=Screen()
-my_screen.exitonclick()
+my_screen.exitonclick """
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("pokemon name",["pikachu","squirtle","charmander"])
+table.add_column("pokemon type",["electric","water","fire"])
+
+print(table)
